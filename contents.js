5200ad3eaf0c59e8af0be0e6e0964e676f933064
DeAndre101 <deandrenelson731@gmail.com>
2020-09-27 04:54:53 -0400

    added small popup window to extension and created a function js file

---
+++ contents.js
@@ -0,0 +1,20 @@
+const parser = new DOMParser()
+
+// const html_parsed = parser.parseFromString()
+
+
+//[get_urlName]: retrieves url from the page when extension is invoked
+
+let current_location = location.href
+
+
+const get_urlName = (loc)=>{
+
+    console.log(loc)
+}
+
+get_urlName(current_location)
+
+
+chrome.permissions.request({
+    permissions: ['tabs'],
